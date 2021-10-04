d544704b1746a74add5381c5a4c14e94c5eb98e7
Swinburn Miranda <smiranda@medallia.com>
2021-10-04 16:18:41 -0700

    change tabs to spaces

--- src/main/java/org/c02e/jpgpj/JcaContextHelper.java
+++ src/main/java/org/c02e/jpgpj/JcaContextHelper.java
@@ -31,139 +31,139 @@ import java.security.Provider;
  * Note: The following class is not thread safe, the security provider should not be changed during PGP operations
  */
 public class JcaContextHelper {
-	private static final Logger log = LoggerFactory.getLogger(JcaContextHelper.class.getName());
-	private static Provider securityProvider = getBcProviderInstance();
+    private static final Logger log = LoggerFactory.getLogger(JcaContextHelper.class.getName());
+    private static Provider securityProvider = getBcProviderInstance();
 
-	private JcaContextHelper() {
-	}
+    private JcaContextHelper() {
+    }
 
-	/**
-	 * Get the security provider which is used for all operations
-	 */
-	public static Provider getSecurityProvider() {
-		return securityProvider;
-	}
+    /**
+     * Get the security provider which is used for all operations
+     */
+    public static Provider getSecurityProvider() {
+        return securityProvider;
+    }
 
-	/**
-	 * Set the security provider to be used for all operations.
-	 */
-	public static void setSecurityProvider(Provider securityProvider) {
-		JcaContextHelper.securityProvider = securityProvider;
-	}
+    /**
+     * Set the security provider to be used for all operations.
+     */
+    public static void setSecurityProvider(Provider securityProvider) {
+        JcaContextHelper.securityProvider = securityProvider;
+    }
 
-	static boolean isSecurityProviderNotNull() {
-		return getSecurityProvider() != null;
-	}
+    static boolean isSecurityProviderNotNull() {
+        return getSecurityProvider() != null;
+    }
 
-	static JcaKeyFingerprintCalculator getJcaKeyFingerprintCalculator() {
-		JcaKeyFingerprintCalculator fingerPrintCalculator = new JcaKeyFingerprintCalculator();
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			fingerPrintCalculator.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return fingerPrintCalculator;
-	}
+    static JcaKeyFingerprintCalculator getJcaKeyFingerprintCalculator() {
+        JcaKeyFingerprintCalculator fingerPrintCalculator = new JcaKeyFingerprintCalculator();
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            fingerPrintCalculator.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return fingerPrintCalculator;
+    }
 
-	static PGPContentVerifierBuilderProvider getPGPContentVerifierBuilderProvider() {
-		JcaPGPContentVerifierBuilderProvider provider = new JcaPGPContentVerifierBuilderProvider();
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			provider.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return provider;
-	}
+    static PGPContentVerifierBuilderProvider getPGPContentVerifierBuilderProvider() {
+        JcaPGPContentVerifierBuilderProvider provider = new JcaPGPContentVerifierBuilderProvider();
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            provider.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return provider;
+    }
 
-	static PGPDigestCalculatorProvider getPGPDigestCalculatorProvider() throws PGPException {
-		JcaPGPDigestCalculatorProviderBuilder builder = new JcaPGPDigestCalculatorProviderBuilder();
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			builder.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return builder.build();
-	}
+    static PGPDigestCalculatorProvider getPGPDigestCalculatorProvider() throws PGPException {
+        JcaPGPDigestCalculatorProviderBuilder builder = new JcaPGPDigestCalculatorProviderBuilder();
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            builder.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return builder.build();
+    }
 
-	static JcePBEDataDecryptorFactoryBuilder getJcePBEDataDecryptorFactoryBuilder() throws PGPException {
-		JcePBEDataDecryptorFactoryBuilder builder =
-				new JcePBEDataDecryptorFactoryBuilder(JcaContextHelper.getPGPDigestCalculatorProvider());
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			builder.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return builder;
-	}
+    static JcePBEDataDecryptorFactoryBuilder getJcePBEDataDecryptorFactoryBuilder() throws PGPException {
+        JcePBEDataDecryptorFactoryBuilder builder =
+                new JcePBEDataDecryptorFactoryBuilder(JcaContextHelper.getPGPDigestCalculatorProvider());
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            builder.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return builder;
+    }
 
-	static JcePublicKeyDataDecryptorFactoryBuilder getJcePublicKeyDataDecryptorFactoryBuilder() {
-		JcePublicKeyDataDecryptorFactoryBuilder builder = new JcePublicKeyDataDecryptorFactoryBuilder();
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			builder.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return builder;
-	}
+    static JcePublicKeyDataDecryptorFactoryBuilder getJcePublicKeyDataDecryptorFactoryBuilder() {
+        JcePublicKeyDataDecryptorFactoryBuilder builder = new JcePublicKeyDataDecryptorFactoryBuilder();
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            builder.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return builder;
+    }
 
-	static PGPDataEncryptorBuilder getPGPDataEncryptorBuilder(int encAlgorithm) {
-		JcePGPDataEncryptorBuilder builder = new JcePGPDataEncryptorBuilder(encAlgorithm);
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			builder.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return builder;
-	}
+    static PGPDataEncryptorBuilder getPGPDataEncryptorBuilder(int encAlgorithm) {
+        JcePGPDataEncryptorBuilder builder = new JcePGPDataEncryptorBuilder(encAlgorithm);
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            builder.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return builder;
+    }
 
-	static JcaKeyBoxBuilder getJcaKeyBoxBuilder() {
-		JcaKeyBoxBuilder builder = new JcaKeyBoxBuilder();
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			builder.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return builder;
-	}
+    static JcaKeyBoxBuilder getJcaKeyBoxBuilder() {
+        JcaKeyBoxBuilder builder = new JcaKeyBoxBuilder();
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            builder.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return builder;
+    }
 
-	static JcePBESecretKeyDecryptorBuilder getJcePBESecretKeyDecryptorBuilder() throws PGPException {
-		JcePBESecretKeyDecryptorBuilder builder =
-				new JcePBESecretKeyDecryptorBuilder(JcaContextHelper.getPGPDigestCalculatorProvider());
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			builder.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return builder;
-	}
+    static JcePBESecretKeyDecryptorBuilder getJcePBESecretKeyDecryptorBuilder() throws PGPException {
+        JcePBESecretKeyDecryptorBuilder builder =
+                new JcePBESecretKeyDecryptorBuilder(JcaContextHelper.getPGPDigestCalculatorProvider());
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            builder.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return builder;
+    }
 
-	static PublicKeyKeyEncryptionMethodGenerator getPublicKeyKeyEncryptionMethodGenerator(PGPPublicKey publicKey) {
-		JcePublicKeyKeyEncryptionMethodGenerator generator = new JcePublicKeyKeyEncryptionMethodGenerator(publicKey);
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			generator.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return generator;
-	}
+    static PublicKeyKeyEncryptionMethodGenerator getPublicKeyKeyEncryptionMethodGenerator(PGPPublicKey publicKey) {
+        JcePublicKeyKeyEncryptionMethodGenerator generator = new JcePublicKeyKeyEncryptionMethodGenerator(publicKey);
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            generator.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return generator;
+    }
 
-	static PBEKeyEncryptionMethodGenerator getPBEKeyEncryptionMethodGenerator(
-			char[] symmetricPassphraseChars,
-			HashingAlgorithm hashingAlgorithm,
-			int workFactor) throws PGPException {
-		PGPDigestCalculatorProvider digestCalculatorProvider = JcaContextHelper.getPGPDigestCalculatorProvider();
-		JcePBEKeyEncryptionMethodGenerator jcePBEKeyEncryptionMethodGenerator = new JcePBEKeyEncryptionMethodGenerator(
-				symmetricPassphraseChars,
-				digestCalculatorProvider.get(hashingAlgorithm.ordinal()),
-				workFactor);
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			jcePBEKeyEncryptionMethodGenerator.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return jcePBEKeyEncryptionMethodGenerator;
-	}
+    static PBEKeyEncryptionMethodGenerator getPBEKeyEncryptionMethodGenerator(
+            char[] symmetricPassphraseChars,
+            HashingAlgorithm hashingAlgorithm,
+            int workFactor) throws PGPException {
+        PGPDigestCalculatorProvider digestCalculatorProvider = JcaContextHelper.getPGPDigestCalculatorProvider();
+        JcePBEKeyEncryptionMethodGenerator jcePBEKeyEncryptionMethodGenerator = new JcePBEKeyEncryptionMethodGenerator(
+                symmetricPassphraseChars,
+                digestCalculatorProvider.get(hashingAlgorithm.ordinal()),
+                workFactor);
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            jcePBEKeyEncryptionMethodGenerator.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return jcePBEKeyEncryptionMethodGenerator;
+    }
 
-	static PGPContentSignerBuilder getPGPContentSignerBuilder(int keyAlgorithmCode, int hashAlgorithmOrdinal) {
-		JcaPGPContentSignerBuilder jcaPGPContentSignerBuilder = new JcaPGPContentSignerBuilder(keyAlgorithmCode, hashAlgorithmOrdinal);
-		if (JcaContextHelper.isSecurityProviderNotNull()) {
-			jcaPGPContentSignerBuilder.setProvider(JcaContextHelper.getSecurityProvider());
-		}
-		return jcaPGPContentSignerBuilder;
-	}
+    static PGPContentSignerBuilder getPGPContentSignerBuilder(int keyAlgorithmCode, int hashAlgorithmOrdinal) {
+        JcaPGPContentSignerBuilder jcaPGPContentSignerBuilder = new JcaPGPContentSignerBuilder(keyAlgorithmCode, hashAlgorithmOrdinal);
+        if (JcaContextHelper.isSecurityProviderNotNull()) {
+            jcaPGPContentSignerBuilder.setProvider(JcaContextHelper.getSecurityProvider());
+        }
+        return jcaPGPContentSignerBuilder;
+    }
 
-	private static Provider getBcProviderInstance() {
-		try {
-			return (Provider) Class.forName("org.bouncycastle.jce.provider.BouncyCastleProvider")
-					.getDeclaredConstructor()
-					.newInstance();
-		} catch (ClassNotFoundException |
-				NoSuchMethodException |
-				InstantiationException |
-				IllegalAccessException |
-				InvocationTargetException e) {
-			log.warn("BouncyCastleProvider class not found on classpath, using default security provider");
-		}
-		return null;
-	}
+    private static Provider getBcProviderInstance() {
+        try {
+            return (Provider) Class.forName("org.bouncycastle.jce.provider.BouncyCastleProvider")
+                    .getDeclaredConstructor()
+                    .newInstance();
+        } catch (ClassNotFoundException |
+                NoSuchMethodException |
+                InstantiationException |
+                IllegalAccessException |
+                InvocationTargetException e) {
+            log.warn("BouncyCastleProvider class not found on classpath, using default security provider");
+        }
+        return null;
+    }
 }
